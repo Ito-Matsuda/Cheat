7b21e8ca7a344fa6d10e4fb056ef5db68d7eeb08
Jose Manuel (Ito) <ito-matsuda@hotmail.com>
2017-03-12 16:16:30 -0400

    Cleared up some random errors

--- game.java
+++ game.java
@@ -2,7 +2,7 @@
  * This is an attempt at automizing the game of cheat
  * I guarantee that this is of my own work
  * Specifics for this implementation can be seen in the text file
- * TODO -> After everything is finished, or when it needs to, optimize the randomInt
+ * TODO -> After everything is finished, or when it needs to, optimize the randomCard
  * 		   Function as it may take a while to do
  * 		-> Could do hashing I suppose
  * 		-> Implement placing doubles (as of right now only places singles)
@@ -29,8 +29,10 @@ public class game {
     public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Enter in number of players: ");
-		// generatePlayers(7); // For now will be tested with 7
-		// goThroughTurns();
+		generatePlayers(7); // For now will be tested with 7
+		cards cardPlaced = playerz[0].placeACard(6); // This really doesnt matter
+		// ^ Person has to place the first card
+		goThroughTurns();
 	}
 	
 	/**
@@ -39,81 +41,114 @@ public class game {
 	 * @param number --> Number of players
 	 */
 	public static void generatePlayers(int number){
-		int cardsEach = (52/number); 
+		int cardsEach = (52/number); // May end up giving more cards than intended
 		// Give a floored number
 		// at the end, just give each player another card until there is no more to be given
 		// use addCard funct in player
 		cards cardsHanded[] = new cards[cardsEach];
+		cards tempCard = generateSingleCard(42);
+//		tempCard.setNumber(3); // this causes an error , must create each object first
+//		cardsHanded[6] = tempCard;
+//		cardsHanded[6].printCardDetails();;
+		// make a cards handed array based on the thing
 		int theCard;
 		int determineSuit;
 		// The empty array of cards to give to each player
-		for (int j = 0; j < 7; j++) { // the 7 is temporary for 7 players
-			for (int i = 0; i < number; i++){
-				// TODO Generate cards here call randomInt
-				theCard = randomInt(0,52);
+		for (int j = 0; j < number; j++) { // to go through the number of players
+			for (int i = 0; i < cardsEach; i++){ // put in cards
+				theCard = randomCard(0,52); // put in a random card
 				if((theCard / 13.0) > 3){
-					cardsHanded[i].setSuit(3);
+					// cardsHanded[i].setSuit(3);
 					determineSuit = 3;
-					System.out.println("Set the suit to Spades");
+					//System.out.println("Set the suit to Spades");
 				}
 				else if ((theCard / 13.0) > 2){
-					cardsHanded[i].setSuit(2);
+					// cardsHanded[i].setSuit(2);
 					determineSuit = 2;
-					System.out.println("Set the suit to Hearts");
+					//System.out.println("Set the suit to Hearts");
 				}
 				else if ((theCard / 13.0) > 1){
-					cardsHanded[i].setSuit(1);
+					// cardsHanded[i].setSuit(1);
 					determineSuit = 1;
-					System.out.println("Set the suit to diamond");
+					//System.out.println("Set the suit to diamond");
 				}
 				else {
-					cardsHanded[i].setSuit(0);
+					// cardsHanded[i].setSuit(0);
 					determineSuit = 0;
-					System.out.println("Set the suit to clubs");
+				//	System.out.println("Set the suit to clubs");
 				}
-				System.out.println("Finished setting the suit");
-				cardsHanded[i].setNumber(theCard - (determineSuit*13));
-				System.out.println("Set the number as " + (theCard - (determineSuit*13)));
+				//System.out.println("The number to put is " + (theCard - (determineSuit*13)));
+				tempCard = generateSingleCard(theCard- (determineSuit*13));
+				cardsHanded[i] = tempCard;
+				//cardsHanded[i].setNumber(theCard - (determineSuit*13));
+				//System.out.println("Set the number as " + (theCard - (determineSuit*13)));
 			}
+		System.out.println("---- Finished setting for player " + j + " -----");
+		for(int k = 0; k <7;k++){
+			System.out.print("Card number: ");
+			cardsHanded[k].printCardDetails();
+		}
 		playerz[j] = new player(cardsHanded); // Make em 
 		Arrays.fill(cardsHanded, null); 
 		// Completely reset the cardsHanded array as we will used them again
 		} // End the outer loop
 	} // End generatePlayers
 	
+	
+	public static cards generateSingleCard(int theCard) {
+		cards card = new cards(theCard);
+		return card;
+	}
 	/**
 	 * Generates a random number given a max and a min
+	 * USED SOLEY FOR CARD GENERATION
 	 * Exact same code from Auto-Avalon
 	 * @param min The lowest number
 	 * @param max The highest number
 	 * @return
 	 */
-	public static int randomInt(int min, int max) {
+	public static int randomCard(int min, int max) {
 		Random rand = new Random();
 		int randomNum;
 		// We need it to execute at least once
 		do {
 			randomNum = rand.nextInt((max - min) + 1) + min;
+			//System.out.println("Generated " + randomNum);
 		} while (usedCards[randomNum] != 0); 
 		// While you have not found something taken up
 		// Super inefficient (it's like random sort lol)
 		// OPTIMIZE LATER, this is just a proof of concept, so it just has to work
 		usedCards[randomNum] = 1;// The array now has something in it and that index is used
 		return randomNum;
-	} // End randomInt
+	} // End randomCard
 
 	/**
 	 * Goes through all the turns until the game ends
 	 */
 	public static void goThroughTurns(){
+		int whoseTurn = 0; // begins at player 1
+		int numberLeftGO = playerz.length / 3; // Once players reduced to a third just end game
 		while(!gameFinished){ // while game is not finished
-			// place card on stack
-			// check if anyone won 
-			// check if anyone calls bull
-				// check pile if so
-					// if bull player who put gets whopped
-					// if not then that person who calls it get whopped
-			// else go to next player and end up at gameFinished again
+			placeCard(whoseTurn); // Place your card if you want to see if funct works, replace with 0
+			// Also covers checking to see if anything is bullshit etc
+			
+			if(checkWin(whoseTurn)){ // Check if someone won
+				numberLeftGO++;
+				if (numberLeftGO == playerz.length){
+					System.out.println("Game is over");
+					System.exit(0);
+				}
+				System.out.println("Sup from goThroughTurns \nNumber until victory is " + 
+						(playerz.length - numberLeftGO));
+			}
+			// else no one has emptied yet and pass over turns 
+			// Controls who goes next below
+			if (whoseTurn == 6){
+				whoseTurn = 0; // reset to the first person
+			}
+			else{
+			whoseTurn++; // go to next player
+			}
 		}
 	} // End goThroughTurns
 	
@@ -122,8 +157,39 @@ public class game {
 	 * @param whichPlayer the player whose making their move
 	 */
 	public static void placeCard(int whichPlayer){
-		
+		int cardNumber = cardStack.peek().getNumber(); // Get the last put number on the pile lmao @.peek()
+		int whoseMemory; // Who will call out the bullshit or not
+		cards cardPlaced = playerz[whichPlayer].placeACard(cardNumber);
+		// add it to everyone's memory
+		// ^IMPORTANT -imagine if player called bs on their own thing
+		for (int i = 0; i < playerz.length; i++){
+			playerz[i].addCardMemory(cardPlaced.getNumber());
+			// Add the 8 or whatever to memory
+		}
+		// call the check memory thing for everyone (kinda redundant as everyone has the same memory)
+		// add some variety somehow later
+		if (whichPlayer == playerz.length){
+			// It's the last person
+			whoseMemory = 0; // Just let this guy decide if it's bs or not
+		}
+		else{
+			whoseMemory = whichPlayer+1; // Let the guy after decide
+		}
+		if (playerz[whoseMemory].checkMemory() == false){ // Call em out
+			if (checkPile(cardPlaced) == true){ // If he isnt bs'ing
+				System.out.println("Player " + whoseMemory + " got whopped, NO LIES");
+				getWhopped(whoseMemory); // Give em the pile lmao
+			}
+			else{
+				System.out.println("Player " + whichPlayer + " lied, get called out son");
+				getWhopped(whichPlayer); // That's what you get for lying boi
+			}
+		} // End calling em out
+		else{ // No bullshit / no call out, just add the card to the pile
+			cardStack.push(cardPlaced);
+		}
 	} // End whichPlayer
+	
 	/**
 	 * This would be called after checkMemory from player thing
 	 * @param theCard The "card" that was placed down
